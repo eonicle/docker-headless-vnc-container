eb91092634e9b939b5b90bda80aa2f02d93db2b9
Evgenyj Afanasyev <evgenyj@e.afanasyev>
2017-04-14 13:00:28 +0300

    added chrome install

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -3,9 +3,11 @@
 set -e
 
 echo "Install Chromium Browser"
-apt-get update 
-apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg google-chrome-stable
 apt-get clean -y
-ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
+#ln -s /usr/bin/chromium-browser /usr/bin/google-chrome 
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
 echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
